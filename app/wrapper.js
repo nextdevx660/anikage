d1ea7489b12c658c9290a89225fe8aee5f2010e8
Sajid Ansari <kirito99999000@gmail.com>
2025-06-15 13:43:10 +0530

    Update wrapper.js

--- app/wrapper.js
+++ app/wrapper.js
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from 'react'
 export default function ClickRedirectWrapper({ children }) {
           const [clickCount, setClickCount] = useState(0);
           const [adsDisabled, setAdsDisabled] = useState(false);
-          const adLink = "https://www.profitableratecpm.com/jhgb2mwtp?key=fb6a85938d48a7692435c75a2aea5d42"; // change to your direct link
+          const adLink = "https://www.profitableratecpm.com/rs0f7zac?key=45ccf57dffd4c77159ca5f69445269d3"; // change to your direct link
           // const adLink = 'https://www.kjdjasd'
           const maxClicks = 2; // Allow 1 or 2 click redirects
 
